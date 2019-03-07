80d9f54a8bc82eeb9690790672c036e9b04fdef6
Nastya-Korsak <48296970+Nastya-Korsak@users.noreply.github.com>
2019-03-07 23:39:37 +0300

    Add files via upload

---
+++ Task_1/Task_1/Line.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Line
+    {
+        public void Search(string str)
+        {
+            string line = null;
+            if (!Check(str)) return;
+
+            for(int i = 0;i < str.Length; i++ )
+            {
+                line = "";
+                line=line + str[i];
+                for (int j = 0; j < str.Length -i-1; j++)
+                {
+                    if (str[i+j] != str[i+1+j])
+                    {
+                        line += str[i+1+j];
+                       Console.WriteLine(line);
+                    }
+                    else break;
+                }
+
+            }
+        }
+
+         private static bool Check(string str)
+        {
+            return (str.Length >= 2) ? true : false;
+        }
+
+    }
+}
