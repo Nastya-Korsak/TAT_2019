823e624f4c9b6c2d57389b9d5b1b11cde3f73900
Nastya-Korsak <48296970+Nastya-Korsak@users.noreply.github.com>
2019-04-12 01:38:21 +0300

    Add files via upload

---
+++ Task_6/Task_6/CommandCountTypes.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_6
+{
+    /// <summary>
+    /// This class defineds command for count types
+    /// </summary>
+    class CommandCountTypes : ICommand
+    {
+        DataBase dataBase;
+        /// <summary>
+        /// Constructor for identification Date Base for work
+        /// </summary>
+        /// <param name="dataBase">Date Base</param>
+        public CommandCountTypes(DataBase dataBase)
+        {
+            this.dataBase = dataBase;
+        }
+
+        /// <summary>
+        /// Method for getting count types
+        /// </summary>
+        public void Execute()
+        {
+            dataBase.GetCountType();
+        }
+    }
+}
---
+++ Task_6/Task_6/ICommand.cs
@@ -0,0 +1,14 @@
+﻿namespace Task_6
+{
+    /// <summary>
+    /// Interface for defining main command's methods
+    /// </summary>
+    interface ICommand
+    {
+        /// <summary>
+        /// Method for start work
+        /// </summary>
+        void Execute();
+
+    }
+}
